37e91f29431d3d3dfb22e55592f6fcf91039baf5
Siddharth Bhat <dbgt4531@gmail.com>
2022-10-24 23:35:33 +0530

    add item functionality working

--- app/src/main/java/com/hobbyproject/shoppingapplicationadmin/MainActivity.java
+++ app/src/main/java/com/hobbyproject/shoppingapplicationadmin/MainActivity.java
@@ -1,14 +1,78 @@
 package com.hobbyproject.shoppingapplicationadmin;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.hobbyproject.shoppingapplicationadmin.databinding.ActivityMainBinding;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        db = FirebaseFirestore.getInstance();
+
+        binding.addItemLayout.setVisibility(View.INVISIBLE);
+
+        binding.addItemButtonCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.addItemLayout.setVisibility(View.VISIBLE);
+                binding.addItemButtonCard.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        binding.cancelLayoutButtonCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.addItemLayout.setVisibility(View.INVISIBLE);
+                binding.addItemButtonCard.setVisibility(View.VISIBLE);
+            }
+        });
+
+        binding.addItemLayoutButtonCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addDataToDatabase();
+            }
+        });
+    }
+
+    public void addDataToDatabase() {
+        Map<String, String> item = new HashMap<>();
+        item.put("name", binding.itemTitleEditText.getText().toString().trim());
+        item.put("brand", binding.itemBrandEditText.getText().toString().trim());
+        item.put("price", binding.itemPriceEditText.getText().toString().trim());
+        item.put("description", binding.itemDescriptionEditText.getText().toString().trim());
+
+        db.collection("items").add(item)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        binding.itemTitleEditText.setText("");
+                        binding.itemBrandEditText.setText("");
+                        binding.itemPriceEditText.setText("");
+                        binding.itemDescriptionEditText.setText("");
+                        Toast.makeText(MainActivity.this, "Item inserted successfully!", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
