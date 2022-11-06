0b7fff135b35b46330be3c09fa50653da957927e
Siddharth Bhat <dbgt4531@gmail.com>
2022-11-06 20:12:16 +0530

    completed delete dialog

--- app/src/main/java/com/hobbyproject/shoppingapplicationadmin/ItemDetailsActivity.java
+++ app/src/main/java/com/hobbyproject/shoppingapplicationadmin/ItemDetailsActivity.java
@@ -3,10 +3,14 @@ package com.hobbyproject.shoppingapplicationadmin;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -28,7 +32,6 @@ public class ItemDetailsActivity extends AppCompatActivity {
         setContentView(view);
 
         db = FirebaseFirestore.getInstance();
-        String id = getIntent().getStringExtra("position");
 
         Picasso.get().load(getIntent().getStringExtra("image")).into(binding.itemImageDetailImageView);
         binding.itemTitleDetailTextView.setText(getIntent().getStringExtra("name"));
@@ -46,14 +49,44 @@ public class ItemDetailsActivity extends AppCompatActivity {
         binding.deleteLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                db.collection("items").document(id).delete()
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                finish();
-                            }
-                        });
+                DeleteDialog dd = new DeleteDialog();
+                dd.showDialog(ItemDetailsActivity.this);
             }
         });
+    }
+
+    public class DeleteDialog {
+        public void showDialog(ItemDetailsActivity itemDetailsActivity) {
+            final Dialog dialog = new Dialog(itemDetailsActivity);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+            dialog.setContentView(R.layout.delete_dialog_box);
+            dialog.show();
+            LinearLayout yes, no;
+            yes = (LinearLayout) dialog.findViewById(R.id.deleteSubmitLayout);
+            no = (LinearLayout) dialog.findViewById(R.id.cancelDeleteLayout);
+
+            yes.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String id = getIntent().getStringExtra("position");
+                    db.collection("items").document(id).delete()
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(ItemDetailsActivity.this, "Your item has been deleted!", Toast.LENGTH_SHORT).show();
+                                    finish();
+                                }
+                            });
+                }
+            });
+
+            no.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
+                }
+            });
+        }
     }
 }
