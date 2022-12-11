19f788fcc5480e17878402087eaa254d99e655b5
Siddharth Bhat <dbgt4531@gmail.com>
2022-12-11 17:21:52 +0530

    image upload done

--- app/src/main/java/com/hobbyproject/shoppingapplicationadmin/MainActivity.java
+++ app/src/main/java/com/hobbyproject/shoppingapplicationadmin/MainActivity.java
@@ -2,6 +2,7 @@ package com.hobbyproject.shoppingapplicationadmin;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -9,6 +10,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 
@@ -21,7 +23,11 @@ import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.hobbyproject.shoppingapplicationadmin.databinding.ActivityMainBinding;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,9 +37,11 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore db;
-    private ActivityMainBinding binding;
     ArrayList<DataModalClass> itemsData;
     ItemsDataAdapterClass adapter;
+    Uri uri;
+    private ActivityMainBinding binding;
+    private int GALLARY_IMAGE_REQUEST=200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,18 +59,18 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ItemsDataAdapterClass(itemsData);
         binding.itemsDisplayRecyclerView.setAdapter(adapter);
         db.collection("items").orderBy("date", Query.Direction.DESCENDING).get()
-                        .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                            @Override
-                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                                List<DocumentSnapshot> itemsList = queryDocumentSnapshots.getDocuments();
-                                for (DocumentSnapshot documentSnapshot : itemsList) {
-                                    DataModalClass dataModalClass = documentSnapshot.toObject(DataModalClass.class);
-                                    dataModalClass.setId(documentSnapshot.getId());
-                                    itemsData.add(dataModalClass);
-                                }
-                                adapter.notifyDataSetChanged();
-                            }
-                        });
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<DocumentSnapshot> itemsList = queryDocumentSnapshots.getDocuments();
+                        for (DocumentSnapshot documentSnapshot : itemsList) {
+                            DataModalClass dataModalClass = documentSnapshot.toObject(DataModalClass.class);
+                            dataModalClass.setId(documentSnapshot.getId());
+                            itemsData.add(dataModalClass);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                });
 
         binding.addItemButtonCard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,52 +87,100 @@ public class MainActivity extends AppCompatActivity {
         binding.cancelLayoutButtonCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.addItemLayout.setVisibility(View.INVISIBLE);
-                binding.addItemButtonCard.setVisibility(View.VISIBLE);
-                Animation scale = AnimationUtils.loadAnimation(MainActivity.this, R.anim.add_item_card_layout_animation_close);
-                binding.addItemLayout.startAnimation(scale);
-                binding.addItemBg.setBackgroundColor(Color.parseColor("#00000000"));
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select your image!"), 101);
+//                binding.addItemLayout.setVisibility(View.INVISIBLE);
+//                binding.addItemButtonCard.setVisibility(View.VISIBLE);
+//                Animation scale = AnimationUtils.loadAnimation(MainActivity.this, R.anim.add_item_card_layout_animation_close);
+//                binding.addItemLayout.startAnimation(scale);
+//                binding.addItemBg.setBackgroundColor(Color.parseColor("#00000000"));
             }
         });
 
         binding.addItemLayoutButtonCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addDataToDatabase();
+                addDataToDatabase(uri);
             }
         });
     }
 
-    public void addDataToDatabase() {
-        Map<String, Object> item = new HashMap<>();
-        item.put("name", binding.itemTitleEditText.getText().toString().trim());
-        item.put("brand", binding.itemBrandEditText.getText().toString().trim());
-        item.put("price", binding.itemPriceEditText.getText().toString().trim());
-        item.put("stock", binding.itemStockEditText.getText().toString().trim());
-        item.put("description", binding.itemDescriptionEditText.getText().toString().trim());
-        item.put("category", binding.itemCategoryEditText.getText().toString().trim());
-        item.put("date", FieldValue.serverTimestamp());
+    public void addDataToDatabase(Uri uri) {
 
-        db.collection("items").add(item)
-                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentReference> task) {
-                        binding.addItemLayout.setVisibility(View.INVISIBLE);
-                        binding.addItemButtonCard.setVisibility(View.VISIBLE);
-                        Animation scale = AnimationUtils.loadAnimation(MainActivity.this, R.anim.add_item_card_layout_animation_close);
-                        binding.addItemLayout.startAnimation(scale);
-                        binding.addItemBg.setBackgroundColor(Color.parseColor("#00000000"));
-                        binding.itemTitleEditText.setText("");
-                        binding.itemBrandEditText.setText("");
-                        binding.itemPriceEditText.setText("");
-                        binding.itemStockEditText.setText("");
-                        binding.itemDescriptionEditText.setText("");
-                        binding.itemCategoryEditText.setText("");
-                        Toast.makeText(MainActivity.this, "Item inserted successfully!", Toast.LENGTH_SHORT).show();
-                        finish();
-                        MainActivity.this.overridePendingTransition(R.anim.nothing,R.anim.nothing);
-                        startActivity(getIntent());
-                    }
-                });
+        binding.addImageCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "CLICKED", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent();
+//                intent.setType("image/*");
+//                intent.setAction(Intent.ACTION_GET_CONTENT);
+//                startActivityForResult(Intent.createChooser(intent, "Select your image!"), 101);
+            }
+        });
+        if (uri != null) {
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference reference = storage.getReference().child( System.currentTimeMillis() + binding.itemTitleEditText.getText().toString() + ".jpg");
+            reference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    reference.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            String file_url = task.getResult().toString();
+
+
+                            Map<String, Object> item = new HashMap<>();
+                            item.put("image", file_url);
+                            item.put("name", binding.itemTitleEditText.getText().toString().trim());
+                            item.put("brand", binding.itemBrandEditText.getText().toString().trim());
+                            item.put("price", binding.itemPriceEditText.getText().toString().trim());
+                            item.put("stock", binding.itemStockEditText.getText().toString().trim());
+                            item.put("description", binding.itemDescriptionEditText.getText().toString().trim());
+                            item.put("category", binding.itemCategoryEditText.getText().toString().trim());
+                            item.put("date", FieldValue.serverTimestamp());
+
+                            db.collection("items").add(item)
+                                    .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentReference> task) {
+                                            binding.addItemLayout.setVisibility(View.INVISIBLE);
+                                            binding.addItemButtonCard.setVisibility(View.VISIBLE);
+                                            Animation scale = AnimationUtils.loadAnimation(MainActivity.this, R.anim.add_item_card_layout_animation_close);
+                                            binding.addItemLayout.startAnimation(scale);
+                                            binding.addItemBg.setBackgroundColor(Color.parseColor("#00000000"));
+                                            binding.itemTitleEditText.setText("");
+                                            binding.itemBrandEditText.setText("");
+                                            binding.itemPriceEditText.setText("");
+                                            binding.itemStockEditText.setText("");
+                                            binding.itemDescriptionEditText.setText("");
+                                            binding.itemCategoryEditText.setText("");
+                                            Toast.makeText(MainActivity.this, "Item inserted successfully!", Toast.LENGTH_SHORT).show();
+                                            finish();
+                                            MainActivity.this.overridePendingTransition(R.anim.nothing, R.anim.nothing);
+                                            startActivity(getIntent());
+
+                                        }
+                                    });
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            uri = data.getData();
+            try {
+                Picasso.get().load(uri).into(binding.firebaseUploadImageView);
+                addDataToDatabase(uri);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
